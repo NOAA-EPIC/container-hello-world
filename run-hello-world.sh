af56999c1b1070ec82af01b15ca7a5ce2a12ec44
Wei Huang <Wei.Huang@noaa.gov>
2025-12-01 08:08:06 -0700

    using one node OK, problem to run two nodes

--- run-hello-world.sh
+++ run-hello-world.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-source /usr/lmod/lmod/init/bash
-module use "/glade/work/huangwei/src/container/global-workflow-cloud/sorc/ufs_model.fd/modulefiles"
-module load ufs_container.intel
-
-export LD_LIBRARY_PATH=${CRAY_MPICH_DIR}/lib-abi-mpich:/opt/cray/pe/lib64:$LD_LIBRARY_PATH:/host/usr/lib64
+export LD_LIBRARY_PATH=$NCAR_LIBFABRIC_ROOT/lib64:${CRAY_MPICH_DIR}/lib-abi-mpich:/opt/cray/pe/lib64:$LD_LIBRARY_PATH:/host/usr/lib64
 
 /gpfs/csfs1/work/huangwei/src/container-hello-world/hello-world.x "$@"
